6deac013905918422d8b23710b5ec465150ab7c0
Pablo Badillo <pabloboysn64@gmail.com>
2022-04-28 10:21:43 -0500

    calculate_expresion

--- Calculadora/Tools.cs
+++ Calculadora/Tools.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
 using System.IO;
 
 namespace Calculadora
 {
     public class Tools
     {
-        public bool paso1 = false;        
+        public bool paso1 = false;
         public string arch_intermed = "";
         public string tabsim = "";
         public string object_program = "";
         public string errors = "";
         public bool is_newFile = false;
+        public string text = "";
+        public string directory = Directory.GetCurrentDirectory();
+        public string path = "";
+        public bool is_constant;
 
         public static Tools tools = new Tools();
+
+        public Tools()
+        {
+            path = directory + "TABSIM.txt";
+        }
         /// <summary>
         /// Convierte un String a Entero hexadecimal
         /// </summary>
@@ -56,6 +67,113 @@ namespace Calculadora
             return r;
         }
 
+        public void Calculate_expression([NotNull] Gramatica_CalculadoraParser.InstruccionContext context, int step)
+        {
+
+        }
+
+        public int value_operand(string operand)
+        {
+            int res = -1;
+            operand = operand.Replace(" ", "");
+            if (operand.All(char.IsDigit))
+            {
+                res = Int32.Parse(operand);
+                if (res > 0 && res < 4096)
+                    is_constant = true;
+                // es un numero decimal
+            }
+            else
+            {
+                if (operand.Substring(operand.Length - 1, 1) == "H" || operand.Substring(operand.Length - 1, 1) == "H")
+                {
+                    string test = operand.Remove(operand.Length - 1);
+                    if (System.Text.RegularExpressions.Regex.IsMatch(test, @"\A\b[0-9a-fA-F]+\b\Z"))
+                    {
+                        res = HexToIntWithOutH(test);
+                        if (res > 0 && res < 4096)
+                            is_constant = true;
+                    }
+                    else
+                    {
+                        if (Read_tabsim(operand))
+                        {
+                            string[] lines_tab_sim = File.ReadAllLines(directory + "TABSIM.txt");
+                            foreach (string line in lines_tab_sim)
+                            {
+                                if (line.Contains(operand))
+                                {
+                                    res = getDirNumConstant(line);
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // error
+                        }
+                    }
+                }
+                else
+                {
+                    if (Read_tabsim(operand))
+                    {
+                        string[] lines_tab_sim = File.ReadAllLines(directory + "TABSIM.txt");
+                        foreach (string line in lines_tab_sim)
+                        {
+                            if (line.Contains(operand))
+                            {
+                                res = getDirNumConstant(line);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // error
+                    }
+                }
+
+            }
+
+            return res;
+        }
+
+        int getDirNumConstant(string line)
+        {
+            string res = "";
+            int indextabsim = 0;
+            while (indextabsim < line.Length)
+            {
+                if (line[indextabsim] == '\t')
+                {
+                    indextabsim++;
+                    while (indextabsim < line.Length)
+                        if (line[indextabsim] != '\t')
+                            res += line[indextabsim++].ToString();
+                        else
+                            break;
+                    break;
+                }
+                indextabsim++;
+            }
+            return Convert.ToInt32(res, 16);
+        }
+
+        public bool Read_tabsim(string symb)
+        {
+            bool is_contained = false;
+            if (File.Exists(path))
+            {
+                text = File.ReadAllText(directory + "TABSIM.txt");
+                if (text.Contains(symb))
+                {
+                    is_contained = true;
+                }
+            }
+            return is_contained;
+        }
+
 
         public void WriteFileProgObj(String reg)
         {
@@ -70,7 +188,7 @@ namespace Calculadora
         {
             string res = "";
             int diff = num_bytes - source.Length;
-            if(diff < 0)
+            if (diff < 0)
                 res = source.Remove(0, Math.Abs(diff));
             else
                 res = source.PadLeft(num_bytes, '0');
